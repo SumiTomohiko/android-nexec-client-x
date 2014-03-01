2a05d70f48023fca52a50d0fdab3aa0fb3357fe6
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2014-03-01 22:17:50 +0900

    Changed the package of MainActivity.

--- src/jp/gr/java_conf/neko_daisuki/nexec_x/activity/MainActivity.java
+++ src/jp/gr/java_conf/neko_daisuki/nexec_x/activity/MainActivity.java
@@ -1,8 +1,12 @@
-package jp.gr.java_conf.neko_daisuki.nexec_x;
+package jp.gr.java_conf.neko_daisuki.nexec_x.activity;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+
+import jp.gr.java_conf.neko_daisuki.nexec_x.R;
+import jp.gr.java_conf.neko_daisuki.nexec_x.R.layout;
+import jp.gr.java_conf.neko_daisuki.nexec_x.R.menu;
 
 public class MainActivity extends Activity {
 
