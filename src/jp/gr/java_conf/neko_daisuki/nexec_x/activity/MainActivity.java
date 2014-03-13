29b644536f2a6431ed7909905fd3b8d22e1e8820
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2014-03-13 13:56:41 +0900

    Arranged coding style, and removed the useless statements.

--- src/jp/gr/java_conf/neko_daisuki/nexec_x/activity/MainActivity.java
+++ src/jp/gr/java_conf/neko_daisuki/nexec_x/activity/MainActivity.java
@@ -1,20 +1,12 @@
 package jp.gr.java_conf.neko_daisuki.nexec_x.activity;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 
 import jp.gr.java_conf.neko_daisuki.nexec_x.R;
-import jp.gr.java_conf.neko_daisuki.nexec_x.R.layout;
-import jp.gr.java_conf.neko_daisuki.nexec_x.R.menu;
 
 public class MainActivity extends Activity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -23,4 +15,9 @@ public class MainActivity extends Activity {
         return true;
     }
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
