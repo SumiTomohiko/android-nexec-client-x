283c0be32b7a1c7ce28e79d8011910285b667634
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2014-04-14 23:13:21 +0900

    Enhanced to call xDraw().

--- src/jp/gr/java_conf/neko_daisuki/nexec_x/widget/XView.java
+++ src/jp/gr/java_conf/neko_daisuki/nexec_x/widget/XView.java
@@ -1,10 +1,16 @@
 package jp.gr.java_conf.neko_daisuki.nexec_x.widget;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.View;
 
+import jp.gr.java_conf.neko_daisuki.android.nexec.client.util.NexecClient;
+
 public class XView extends View {
+
+    private NexecClient mClient;
 
     public XView(Context context) {
         super(context);
@@ -16,5 +22,18 @@ public class XView extends View {
 
     public XView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+    }
+
+    public void setNexecClient(NexecClient client) {
+        mClient = client;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Bitmap bitmap = mClient.xDraw();
+        if (bitmap == null) {
+            return;
+        }
+        canvas.drawBitmap(bitmap, 0.0f, 0.0f, null);
     }
 }
